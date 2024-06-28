883b31c6e54bb7bd888530f939c56b80c9b1471b
yasir1209 <yasirswati727@gmail.com>
2024-06-28 13:16:41 -0700

    dashboard small icon updated

---
+++ src/Pages/Dashboard/Icons.tsx
@@ -0,0 +1,199 @@
+export const BookingIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="16"
+        viewBox="0 0 18 16"
+        fill="none"
+        className="injected-svg"
+        data-src="/icon/tracking/milestone/milestone-at-shipper.svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          d="M10.1916 1.16895H6.78735V7.97746H10.1916M10.1916 1.16895H13.5959V7.97746H10.1916M10.1916 1.16895V7.97746"
+          stroke="currentColor"
+          stroke-width="0.75"
+        ></path>
+        <path
+          d="M13.1192 10.4966L14.9234 9.74762C15.7064 9.40719 16.6255 9.74762 17 10.5306L10.2255 14.5476C9.74894 14.82 9.20426 14.888 8.69362 14.7859L4.54043 13.7306C4.23404 13.6625 3.92766 13.6625 3.58723 13.7306L1 14.3093"
+          stroke="currentColor"
+          stroke-width="0.75"
+        ></path>
+        <path
+          d="M7.12766 12.0626H11.383C11.8596 12.0626 12.234 11.6881 12.234 11.2115C12.234 10.7349 11.8596 10.3605 11.383 10.3605H8.82979L6.44681 9.64557C5.76596 9.44132 5.05106 9.40728 4.37021 9.57749L1 10.3605"
+          stroke="currentColor"
+          stroke-width="0.75"
+        ></path>
+      </svg>
+    </>
+  );
+};
+export const BookingConfirmation = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="16"
+        viewBox="0 0 40 41"
+        fill="none"
+        className="injected-svg"
+        data-src="/icon/tracking/milestone/milestone-booking-confirmed.svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          d="M33.9833 11.4808L18.8278 2.73083L3.67236 11.4808V28.9808L18.8278 37.7308M33.9833 11.4808L18.8278 21.3246M33.9833 11.4808V20.2308M18.8278 37.7308V21.3246M18.8278 37.7308L22.108 35.837M18.8278 21.3246L4.60906 12.5746M25.3903 15.8558L11.1716 7.10584M27.5778 31.1683L29.7653 33.3558L36.3278 26.7933"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+      </svg>
+    </>
+  );
+};
+export const TransitShipperIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="16"
+        viewBox="0 0 16 17"
+        fill="none"
+        className="injected-svg"
+        data-src="/icon/information/in-transit.svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          d="M7.99997 7.01777V12.3511M5.59998 9.87828C3.25451 10.2739 1.59998 11.232 1.59998 12.3511C1.59998 13.8239 4.46535 15.0178 7.99997 15.0178C11.5346 15.0178 14.4 13.8239 14.4 12.3511C14.4 11.232 12.7454 10.2739 10.4 9.87828M10.1333 4.35111C10.1333 5.52931 9.17818 6.48444 7.99997 6.48444C6.82177 6.48444 5.86664 5.52931 5.86664 4.35111C5.86664 3.1729 6.82177 2.21777 7.99997 2.21777C9.17818 2.21777 10.1333 3.1729 10.1333 4.35111Z"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>
+      </svg>
+    </>
+  );
+};
+export const OriginHandIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="16"
+        viewBox="0 0 16 14"
+        fill="none"
+        className="injected-svg"
+        data-src="/icon/information/on-hand-origin.svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <g id="Group">
+          <path
+            id="Rectangle_4598_1_-491"
+            d="M6.08572 0.783203H3.21338V6.52788H6.08572M6.08572 0.783203H8.95806V6.52788H6.08572M6.08572 0.783203V6.52788"
+            stroke="currentColor"
+            stroke-width="0.75"
+          ></path>
+          <path
+            id="TracÃ©_163079_1_-492"
+            d="M10.8756 9.72395L12.3979 9.09204C13.0585 8.8048 13.8341 9.09204 14.15 9.75268L8.43407 13.142C8.03194 13.3718 7.57237 13.4293 7.14151 13.3431L3.63726 12.4527C3.37875 12.3952 3.12024 12.3952 2.833 12.4527L0.650024 12.941"
+            stroke="currentColor"
+            stroke-width="0.75"
+          ></path>
+          <path
+            id="TracÃ©_163080_1_-493"
+            d="M5.82024 11.0452H9.41066C9.81279 11.0452 10.1287 10.7293 10.1287 10.3271C10.1287 9.925 9.81279 9.60904 9.41066 9.60904H7.25641L5.24577 9.00585C4.6713 8.83351 4.06811 8.80478 3.49364 8.9484L0.650024 9.60904"
+            stroke="currentColor"
+            stroke-width="0.75"
+          ></path>
+          <path
+            id="Vector-494"
+            d="M11.16 5.47767L11.8939 3.19196L11.16 0.90625L15.6819 3.19196L11.16 5.47767Z"
+            fill="currentColor"
+          ></path>
+        </g>
+      </svg>
+    </>
+  );
+};
+
+export const TransitIcon = () => {
+  return (
+    <>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="16"
+        viewBox="0 0 20 21"
+        fill="none"
+        className="injected-svg"
+        data-src="/icon/information/origin-destination.svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+      >
+        <path
+          d="M0 20.4999L0.917334 17.6428L0 14.7856L5.65228 17.6428L0 20.4999Z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M17.1737 18.0509H6.95641C6.71631 18.0509 6.52162 17.8681 6.52162 17.6427C6.52162 17.4173 6.71631 17.2346 6.95641 17.2346H17.1737C17.8727 17.2346 18.5187 16.8845 18.8683 16.3162C19.2177 15.7478 19.2177 15.0478 18.8683 14.4794C18.5187 13.9111 17.8727 13.561 17.1737 13.561H3.69523C2.68552 13.561 1.75264 13.0554 1.24778 12.2345C0.742927 11.4136 0.742927 10.4023 1.24778 9.58144C1.75264 8.76054 2.68552 8.25488 3.69523 8.25488H13.9126C14.1527 8.25488 14.3474 8.43765 14.3474 8.66305C14.3474 8.88844 14.1527 9.07121 13.9126 9.07121H3.69523C2.99629 9.07121 2.35031 9.42124 2.00069 9.9896C1.65131 10.5579 1.65131 11.258 2.00069 11.8263C2.35027 12.3947 2.99624 12.7447 3.69523 12.7447H17.1737C18.1835 12.7447 19.1163 13.2504 19.6212 14.0713C20.1261 14.8922 20.1261 15.9034 19.6212 16.7243C19.1163 17.5452 18.1835 18.0509 17.1737 18.0509Z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M15.218 7.84693C14.9779 7.84693 14.7832 7.66417 14.7832 7.43877V0.908163C14.7832 0.682764 14.9779 0.5 15.218 0.5C15.4581 0.5 15.6528 0.682764 15.6528 0.908163V7.43877C15.6528 7.547 15.607 7.65087 15.5255 7.7274C15.4439 7.80392 15.3333 7.84693 15.218 7.84693Z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M15.2178 4.1735H19.5657L18.2613 2.55727L19.5657 0.908203H15.2178V4.1735Z"
+          fill="currentColor"
+        ></path>
+        <path
+          d="M16.5218 8.66321C16.5218 9.33941 15.9378 9.8877 15.2175 9.8877C14.4971 9.8877 13.9131 9.33941 13.9131 8.66321C13.9131 7.98701 14.4971 7.43872 15.2175 7.43872C15.9378 7.43872 16.5218 7.98701 16.5218 8.66321Z"
+          fill="currentColor"
+        ></path>
+      </svg>
+    </>
+  );
+};
+
+export const AtdistinationIcon = () => {
+    return (
+      <>
+        <svg xmlns="http://www.w3.org/2000/svg" width="15" height="14" viewBox="0 0 15 14" fill="none" className="injected-svg" data-src="/icon/information/on-hand-destination.svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <g id="Group">
+    <path id="Rectangle_4598_1_-575" d="M5.88552 0.783203H3.01318V6.52788H5.88552M5.88552 0.783203H8.75786V6.52788H5.88552M5.88552 0.783203V6.52788" stroke="currentColor" stroke-width="0.75"></path>
+    <path id="Vector-576" d="M11.374 6.31895C11.1819 6.31895 11.0261 6.17274 11.0261 5.99242V0.767936C11.0261 0.587617 11.1819 0.441406 11.374 0.441406C11.566 0.441406 11.7218 0.587617 11.7218 0.767936V5.99242C11.7218 6.07901 11.6851 6.1621 11.6199 6.22332C11.5547 6.28455 11.4662 6.31895 11.374 6.31895Z" fill="currentColor"></path>
+    <path id="Vector_2-577" d="M11.374 3.38031H14.8523L13.8089 2.08732L14.8523 0.768066H11.374V3.38031Z" fill="currentColor"></path>
+    <path id="TracÃ©_163079_1_-578" d="M10.6755 9.72395L12.1978 9.09204C12.8585 8.8048 13.634 9.09204 13.95 9.75268L8.23399 13.142C7.83187 13.3718 7.37229 13.4293 6.94144 13.3431L3.43719 12.4527C3.17867 12.3952 2.92016 12.3952 2.63293 12.4527L0.449951 12.941" stroke="currentColor" stroke-width="0.75"></path>
+    <path id="TracÃ©_163080_1_-579" d="M5.62016 11.0457H9.21059C9.61272 11.0457 9.92867 10.7297 9.92867 10.3276C9.92867 9.92548 9.61272 9.60953 9.21059 9.60953H7.05633L5.0457 9.00634C4.47123 8.83399 3.86804 8.80527 3.29357 8.94889L0.449951 9.60953" stroke="#currentColor" stroke-width="0.75"></path>
+  </g>
+</svg>
+      </>
+    );
+  };
+
+  export const DeliveryIcon = () => {
+    return (
+      <>
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 17 17" fill="none" className="injected-svg" data-src="/icon/information/package-delivery.svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <g id="package-delivery">
+    <path id="Vector-590" d="M3.79644 5.13931L9.82083 1.66113L15.8452 5.13931V12.0957L9.82083 15.5739M3.79644 5.13931L9.82083 9.05227M3.79644 5.13931V7.67163M9.82083 15.5739V9.05227M9.82083 15.5739L7.63764 14.1633M9.82083 9.05227L15.4729 5.57409M7.21219 6.87841L12.8642 3.40022M5.77087 12.085H3.81412H2.61191M4.47857 9.91067H3.3109H1.35469M4.47857 14.4155H2.74119H1.35469" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+  </g>
+</svg>
+      </>
+    );
+  };
+
+  export const DeliveredIcon = () => {
+    return (
+      <>
+<svg xmlns="http://www.w3.org/2000/svg" width="18" height="16" viewBox="0 0 16 17" fill="none" className="injected-svg" data-src="/icon/tracking/milestone/milestone-delivered-2.svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+  <path d="M8 12.6763V6.42627V1.73877L12.2188 5.02002L8.46875 7.83252M5.1875 9.77843C2.43891 10.242 0.5 11.3648 0.5 12.6763C0.5 14.4022 3.85786 15.8013 8 15.8013C12.1421 15.8013 15.5 14.4022 15.5 12.6763C15.5 11.3648 13.5611 10.242 10.8125 9.77843" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"></path>
+</svg>
+      </>
+    );
+  };
+  
+  
